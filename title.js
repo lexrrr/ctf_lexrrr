30578d623fd9ac0cbcbe316b2ee6a389470abaa9
R R <100893320+lexrrr@users.noreply.github.com>
2023-02-17 02:41:44 +0600

    Delete title.js

--- title.js
+++
@@ -1,7 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Come Back ;("
-});
-window.addEventListener("focus", ()=>{
-    document.title = docTitle;
-});
